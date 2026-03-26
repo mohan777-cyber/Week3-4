4022f826181fc1cc4be3c57a0cd4936282aa73eb
Mohan777-cyber <mohanpratap777@gmail.com>
2026-03-26 23:27:05 +0530

    [mohan] added:created a java program to check the odd number

---
+++ level2/OddEvenNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class OddEvenNumber{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+
+            for (int i = 1; i <= number; i++) {
+
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even");
+                } 
+                else {
+                    System.out.println(i + " is odd");
+                }
+
+            }
+
+        } 
+        else {
+            System.out.println("The number " + number + " is not a natural number");
+        }
+
+        sc.close();
+    }
+}
